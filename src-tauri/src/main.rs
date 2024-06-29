ec85bc4cd85da58100d6b89b3a9108258d98224b
SkaaRJik <filippowvitia@yandex.ru>
2024-06-29 22:05:07 +0800

    reailize parse 1 url link

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,12 @@
 extern crate sxd_document;
 
 use std::error::Error;
-use std::future::Future;
-use std::pin::Pin;
+use std::sync::{Arc, Mutex};
+
+use rand::Rng;
 use reqwest::header::{ACCEPT, CONTENT_TYPE};
 use reqwest::StatusCode;
-use std::sync::Arc;
-use futures_util::future::join_all;
-use futures_util::lock::Mutex;
-use rand::Rng;
-use reqwest;
+use scraper::Html;
 use tauri::Manager;
 
 #[tauri::command]
@@ -19,22 +16,22 @@ fn my_custom_command() {
     println!("I was invoked from JS!");
 }
 
-async fn get_document_html(client: &reqwest::Client, url: String) -> Result<scraper::Html, Box<dyn Error>> {
+async fn get_document_html(url: String) -> Result<Html, Box<dyn Error>> {
+    let client = reqwest::Client::new();
     let response = client
         .get(url)
         .header(CONTENT_TYPE, "text/html; charset=utf-8")
         .header(ACCEPT, "text/html; charset=utf-8")
         .send()
-        .await
-        .unwrap();
+        .await?;
 
     let raw_html = match response.status() {
-        StatusCode::OK => response.text().await.unwrap(),
+        StatusCode::OK => response.text().await?,
         _ => panic!("Something went wrong"),
     };
 
-    let document = scraper::Html::parse_document(&raw_html);
-    return Ok(document);
+    let document = Html::parse_document(&raw_html);
+    Ok(document)
 }
 
 async fn get_num_of_pages(resolution: String) -> Result<(usize, usize, usize), Box<dyn Error>> {
@@ -47,15 +44,14 @@ async fn get_num_of_pages(resolution: String) -> Result<(usize, usize, usize), B
         .header(CONTENT_TYPE, "text/html; charset=utf-8")
         .header(ACCEPT, "text/html; charset=utf-8")
         .send()
-        .await
-        .unwrap();
+        .await?;
 
     let raw_html = match response.status() {
-        StatusCode::OK => response.text().await.unwrap(),
+        StatusCode::OK => response.text().await?,
         _ => panic!("Something went wrong"),
     };
 
-    let document = scraper::Html::parse_document(&raw_html);
+    let document = Html::parse_document(&raw_html);
     let pagination_selector =
         scraper::Selector::parse(".pagination_container > div > ul > li:nth-last-child(2)")
             .unwrap();
@@ -73,91 +69,62 @@ async fn get_num_of_pages(resolution: String) -> Result<(usize, usize, usize), B
     }
     Err(format!("Can't calculate pages").into())
 }
+
 async fn get_imgs_links(
     links: Arc<Mutex<Vec<String>>>,
     pages: Arc<u32>,
-    resolution: String,
-    download_folder: String,
+    resolution: Arc<String>,
+    download_folder:  Arc<String>,
 ) -> Result<String, Box<dyn Error>> {
-    let client = reqwest::Client::new();
-    /*loop {*/
-        
+    loop {
         let pic_page = rand::thread_rng().gen_range(0..*pages);
         let page_url = format!(
             "https://hdqwalls.com/category/games-wallpapers/{resolution}/page/{pic_page}",
-            resolution = resolution,
+            resolution = *resolution,
             pic_page = pic_page
         );
 
-        let document = get_document_html(&client, page_url.clone()).await.unwrap();
+        let document = get_document_html(page_url).await.unwrap();
 
-        let pictures_selector = scraper::Selector::parse(format!("a.caption:nth-child({})", 1).as_str()).unwrap();
+        let picture_list_selector =
+            scraper::Selector::parse("a.caption")
+                .unwrap();
+        let pictures_per_page = document.select(&picture_list_selector).count();
+        let picture_index = rand::thread_rng().gen_range(1..pictures_per_page+1);
 
-        if let Some(pic_page_element) = document.select(&pictures_selector).next() {
-            let pic_url = pic_page_element.value().attr("href").unwrap();
-            println!("{}", pic_url);
-            let mut links_guard = links.lock().await;
-            if !links_guard.contains(&pic_url.to_string()) {
-                links_guard.push(pic_url.to_string());
-                println!("added {}", pic_url);
-                return Ok("Added".to_string());
-            } else {
-                return Err(format!("Can't calculate pages").into());
-            }
+        let pictures_selector = scraper::Selector::parse(format!("a.caption:nth-child({})", picture_index).as_str()).unwrap();
+        let pic_page_element = document.select(&pictures_selector).next().unwrap();
+        let pic_url = pic_page_element.value().attr("href").unwrap().to_string();
+        println!("{}", pic_url);
+        // Lock the mutex to get access to the vector
+        let mut links_guard = links.lock().unwrap();
+        if !links_guard.contains(&pic_url) {
+            links_guard.push(pic_url.clone());
+            println!("added {}", pic_url);
+            return Ok("Added".to_string());
+        } else {
+            return Err("URL already exists".into());
         }
-    return Err(format!("Can't calculate pages").into());
-    /*}*/
-   
+    }
+    Err("No picture found".into())
 }
 
 async fn download_pictures(num_of_pics: i32, resolution: Arc<String>, img_folder: Arc<String>) -> Result<String, Box<dyn Error>> {
-    /*match get_num_of_pages(resolution.clone().to_string()).await {
-        Ok((pages, pictures_per_page, max_value)) => {
-
-        }
-        Err(err) => {
-            println!("{}", err.to_string())
-        }
-    }*/
-
-
     let links: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(vec![]));
-    let arcPages = Arc::new(30);
-    
-    /*let arcPages: Arc<usize> = Arc::new(pages);*/
+    let arc_pages = Arc::new(30);
 
-    
     match get_imgs_links(
-        links.clone(),
-        Arc::clone(&arcPages),
-        resolution.clone().to_string(),
-        img_folder.clone().to_string(),
+        Arc::clone(&links),
+        Arc::clone(&arc_pages),
+        Arc::clone(&resolution),
+        Arc::clone(&img_folder),
     ).await {
-        Ok(str) => {
-            println!("{}", str.to_string())
-        }
-        Err(err) => {
-            println!("{}", err.to_string())
-        }
+        Ok(msg) => println!("{}", msg),
+        Err(err) => println!("{}", err),
     }
-    /*
-    let mut tasks = vec![];
-    for _i in 0..num_of_pics {
-        tasks.push(
-           get_imgs_links(
-                links.clone(),
-                Arc::clone(&arcPages),
-                resolution.clone().to_string(),
-                img_folder.clone().to_string(),
-            )
-        )
-    }
-
-    let res = join_all(tasks).await;*/
 
     Ok("Good".to_string())
 }
-
 
 fn setup<'a>(app: &'a mut tauri::App) -> Result<(), Box<dyn Error>> {
     let window = app.get_window("main").unwrap();
@@ -165,17 +132,15 @@ fn setup<'a>(app: &'a mut tauri::App) -> Result<(), Box<dyn Error>> {
     {
         window.open_devtools();
     }
-    // This one
-
 
     tauri::async_runtime::spawn(async move {
-        // also added move here
+        // added move here
         let resolution = Arc::new(String::from("2560x1440"));
         let img_folder = Arc::new(String::from("img"));
         match download_pictures(10, resolution, img_folder).await {
-            Ok(mes) => {
-                window.emit("download-progress", 100);
-                println!("{}", mes);
+            Ok(msg) => {
+                window.emit("download-progress", 100).unwrap();
+                println!("{}", msg);
             }
             Err(err) => {
                 println!("{}", err);
@@ -185,11 +150,9 @@ fn setup<'a>(app: &'a mut tauri::App) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-
 fn main() {
     tauri::Builder::default()
         .setup(setup)
-        /*.invoke_handler(tauri::generate_handler![js2rs])*/
         .run(tauri::generate_context!())
         .expect("Box<dyn Error> while running tauri application");
 
